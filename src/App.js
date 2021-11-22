4a4b2b65f8632fb96da529607ddc334d47621d88
SolbiShin <sbshin.dev@gmail.com>
2021-11-22 22:50:19 +0900

    react-router-dom 버전 변경(v5) && 서브 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 
 const App = () => {
@@ -16,18 +16,13 @@ const App = () => {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/sbshin">sbshin 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/gildong">gildong 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
       <hr />
-      <Routes>
-        <Route path="/" component={Home} exact={true} />
-        <Route path={['/about', '/info']} component={About} />
-        <Route path="/profile/:username" component={Profile} />
-      </Routes>
+      <Route path="/" component={Home} exact={true} />
+      <Route path={['/about', '/info']} component={About} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 }
