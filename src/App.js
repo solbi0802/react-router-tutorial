0384f7ef9e0b1cdb674022e03ff02f2d3e5408ad
SolbiShin <sbshin.dev@gmail.com>
2021-11-22 23:09:32 +0900

    Switch, NavLink 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 import Profiles from './Profiles';
@@ -24,10 +24,20 @@ const App = () => {
         </li>
       </ul>
       <hr />
-      <Route path="/" component={Home} exact={true} />
-      <Route path={['/about', '/info']} component={About} />
-      <Route path="/profiles" component={Profiles} />
-      <Route path="/history" component={HistorySample} />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path={['/about', '/info']} component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          // path를 따로 지정  안하면 모든 상황에 렌더링됨
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다 :( Page Not Found(404)</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )} />
+      </Switch>
     </div>
   );
 }
--- src/Profiles.js
+++ src/Profiles.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import Profile from './Profile';
 
 
 const Profiles = () => {
+    const activeStyle = {
+        background: 'black',
+        color: 'yellow'
+    };
 
     return (
         <div>
             <h3>사용자 목록:</h3>
             <ul>
                 <li>
-                    <Link to="/profiles/sbshin">sbshin</Link>
+                    <NavLink activeStyle={activeStyle} to="/profiles/sbshin">sbshin</NavLink>
                 </li>
                 <li>
-                    <Link to="/profiles/gildong">gildong</Link>
+                    <NavLink activeStyle={activeStyle} to="/profiles/gildong">gildong</NavLink>
                 </li>
             </ul>
 
